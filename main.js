2966632645914e266f9fe2d49fe1d5321a067290
Mugunth148 <Mugunth148@gmail.com>
2023-07-08 14:24:01 +0530

    Completed Desktop version

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+// getting the button and audio element
+
 var button = document.getElementById("button");
 var audio = document.getElementById("audio");
+
+// play/pause function using the audio element
 
 button.addEventListener("click", function() {
   if (audio.paused) {
     audio.play();
-    button.innerHTML = "Pause";
+    button.innerHTML = "Stop";
   } else {
     audio.pause();
-    button.innerHTML = "Play";
+    button.innerHTML = "Buzz";
   }
 });
